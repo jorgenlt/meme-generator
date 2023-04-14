384b3f3870ced0a4d6d0426ebd395a818edb5518
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-14 10:42:18 +0200

    git init

---
+++ src/pages/components/Header.js
@@ -0,0 +1,15 @@
+
+export default function Header() {
+    return (
+        <div id="header">
+            <a href="https://github.com/jorgenlt/meme-generator" target="_blank" rel="norefferer" id="memeface">
+                <img
+                    src="troll-face.png"
+                    alt="troll face"
+                    height="26px"
+                />
+                <h1>Meme Generator</h1>
+            </a>
+        </div>
+    )
+}
